53fd9a53a001dc4840b7711faa04e9f26fee7ef9
BelleLQ <59607658+BelleLQ@users.noreply.github.com>
2022-01-05 03:54:54 -0500

    make it reponsive to mobile devices

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,24 @@ const Footer = () => {
     return (
         <footer>
             <div className="container-fluid footer d-flex justify-content-center flex-column">
-                <div className="d-flex p-1">
-                    <div className="footer-text">Privacy Policy</div>
-                    <div className="footer-text">Terms & Conditions</div>
-                        <div className="ms-auto footer-text"><a href="https://www.facebook.com/hachu.select.ca" target="_blank"><FaFacebookF className="footer-text"/></a></div>
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="footer-text">Privacy Policy</div>
+                        <div className="footer-text">Terms & Conditions</div>
+                    </div>
+
+                    <div className="col-12 col-md-6 footer-right-column">
+                        <div className="footer-text "><a href="https://www.facebook.com/hachu.select.ca" target="_blank"><FaFacebookF className="footer-text"/></a></div>
                         <div className="footer-text"><a href="https://www.instagram.com/hachu_select.ca/" target="_blank"><FaInstagram className="footer-text" /></a></div>
                     </div>
-                    <div className="d-flex p-1">
-                        <div className="me-auto footer-text">hachu.canada@gmail.com</div>
-                        <div className="footer-text">© HaChu.  A better way of living.</div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <div className="footer-text">hachu.canada@gmail.com</div>
+                        </div>
+                        <div className="col-12 col-md-6 footer-right-column">
+                            <div className="footer-text">© HaChu.  A better way of living.</div>
+                        </div>
                 </div>
             </div>
         </footer>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,17 +4,22 @@ const Hero = () => {
     return (
             <div className="container-fluid p-0">
                 <div className="row">
-                    <div className="col-4 text-start my-auto px-5">
+                    <div className="col-md-4 col-12 my-auto px-5 hero-title hero-md">
                         <p className="font-Pacifico">A better way of living</p>
                         <p className="hero-text">High-quality Canadian merchandise</p>
                         <p className="hero-text">Less chemistry, more nature</p>
                     </div>
-                    <div className="col-8">
+                    <div className="col-md-8 col-12">
                         <img
                             src="https://hachu-canada.com/wp-content/uploads/2021/10/IMG_6143-2.jpg"
                             alt="new"
                             className="hero-img"
                         />
+                    </div>
+                    <div className="col-md-4 col-12 my-auto px-5 hero-title hero-sm">
+                        <p className="font-Pacifico">A better way of living</p>
+                        <p className="hero-text">High-quality Canadian merchandise</p>
+                        <p className="hero-text">Less chemistry, more nature</p>
                     </div>
                 </div>
             </div>
